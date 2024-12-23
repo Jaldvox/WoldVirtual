af73b3ec799de2c360b83c8970b9beba4012d625
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-23 13:43:45 +0100

    Update BK_ST1.py

--- public/STM/BK_ST1.py
+++ public/STM/BK_ST1.py
@@ -11,3 +11,7 @@ if __name__ == '__main__':
     registrar_usuario(username, password)
     print(f'Usuario {username} registrado con éxito.')
 
+ if verificar_credenciales(username, password):
+        print('Credenciales verificadas con éxito.')
+    else:
+        print('Error al verificar credenciales.')
