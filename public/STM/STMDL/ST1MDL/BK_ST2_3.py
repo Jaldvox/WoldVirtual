fb1705630b1b12766cf802063b07e772ac1ca719
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-23 17:05:08 +0100

    Update BK_ST2_3.py

--- public/STM/STMDL/ST1MDL/BK_ST2_3.py
+++ public/STM/STMDL/ST1MDL/BK_ST2_3.py
@@ -1,3 +1,6 @@
+import hashlib
+import datetime
+
 class Blockchain:
     def __init__(self):
         self.chain = []
@@ -6,9 +9,15 @@ class Blockchain:
     def crear_bloque_genesis(self):
         genesis_block = {
             'index': 0,
-            'timestamp': '2024-08-20 00:00:00',
+            'timestamp': str(datetime.datetime.now()),
             'data': 'Bloque Génesis',
-            'previous_hash': '0'
+            'previous_hash': '0',
+            'hash': self.hash_block({
+                'index': 0,
+                'timestamp': str(datetime.datetime.now()),
+                'data': 'Bloque Génesis',
+                'previous_hash': '0'
+            })
         }
         self.chain.append(genesis_block)
 
@@ -16,26 +25,31 @@ class Blockchain:
         previous_block = self.chain[-1]
         new_block = {
             'index': len(self.chain),
-            'timestamp': '2024-08-20 00:00:00',
+            'timestamp': str(datetime.datetime.now()),
             'data': data,
-            'previous_hash': self.hash(previous_block)
+            'previous_hash': previous_block['hash'],
+            'hash': ''
         }
+        new_block['hash'] = self.hash_block(new_block)
         self.chain.append(new_block)
 
     def obtener_informacion_cadena(self):
         informacion = {
             'longitud': len(self.chain),
-            'bloques': [block for block in self.chain],
+            'bloques': self.chain
         }
         return informacion
 
-    def hash(self, block):
-        return str(block)  # Esto es solo un ejemplo, usa una función de hash real
+    def hash_block(self, block):
+        block_string = f"{block['index']}{block['timestamp']}{block['data']}{block['previous_hash']}".encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     def validar_cadena(self):
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
             previous_block = self.chain[i - 1]
-            if current_block['previous_hash'] != self.hash(previous_block):
+            if current_block['previous_hash'] != previous_block['hash']:
+                return False
+            if current_block['hash'] != self.hash_block(current_block):
                 return False
         return True
