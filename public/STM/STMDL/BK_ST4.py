1ee515dec2bb284dd1b9d170620f0f32745338e3
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-23 14:35:50 +0100

    Update BK_ST4.py

--- public/STM/STMDL/BK_ST4.py
+++ public/STM/STMDL/BK_ST4.py
@@ -1,7 +1,10 @@
 def manejar_accion(usuario, accion):
+    """
+    Realiza una acción específica para el usuario.
+    """
     if accion == "explorar":
         print(f"Bienvenido/a {usuario} al entorno de exploración.")
     elif accion == "intercambiar":
         print(f"Realizando intercambio para {usuario}.")
     else:
-        print("Acción no reconocida.")
+        print(f"Error: Acción '{accion}' no reconocida.")
