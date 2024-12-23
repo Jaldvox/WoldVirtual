1c995d18c5d410c56b7eaa4311e286b8596b6dc4
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-23 14:32:09 +0100

    Update BK_ST2.py

--- public/STM/STMDL/BK_ST2.py
+++ public/STM/STMDL/BK_ST2.py
@@ -1,3 +1,15 @@
+import hashlib
+from BK_Usuarios import usuarios
+
 def registrar_usuario(username, password):
+    """
+    Registra un nuevo usuario con su contraseña.
+    Devuelve True si el usuario fue registrado, 
+    False si ya existe.
+    
+    """
+    if username in usuarios:
+        return False
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     usuarios[username] = hashed_password
+    return True
