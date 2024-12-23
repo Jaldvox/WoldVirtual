b8a6c37d7eff68a6dca5a4ce0bf09df08ecf9eee
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-23 15:41:27 +0100

    Update BK_ST3.py

--- public/STM/STMDL/BK_ST3.py
+++ public/STM/STMDL/BK_ST3.py
@@ -1,11 +1,12 @@
+# modulos/BK_ST3.py
+
 import hashlib
-from BK_Usuarios import usuarios
+from STMDL.BK_Usuarios import usuarios
 
 def verificar_credenciales(username, password):
     """
     Verifica si las credenciales del usuario son correctas.
     Devuelve True si coinciden, False en caso contrario.
-    
     """
     if username not in usuarios:
         print(f'Error: El usuario "{username}" no existe.')
