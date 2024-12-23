6d8ae81fca83f372ca1d8d2b66fd425c066e68e9
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-23 20:45:37 +0000

    add

--- public/BK_CPSGR/app.py
+++ public/BK_CPSGR/app.py
@@ -1,9 +1,9 @@
-from usuarios   import registrar_usuario, verificar_credenciales, manejar_accion
-from recursos   import RecursosUsuario, asignar_recursos_a_usuario, MonitoreoRecursos
-from blockchain import Blockchain
-from database   import conectar_base_datos
-from compresion import comprimir_y_guardar_datos, cargar_y_descomprimir_datos
-from servidor   import app, socketio
+from BK_CPSGR.usuarios   import registrar_usuario, verificar_credenciales, manejar_accion
+from BK_CPSGR.recursos   import RecursosUsuario, asignar_recursos_a_usuario, MonitoreoRecursos
+from BK_CPSGR.blockchain import Blockchain
+from BK_CPSGR.database  import conectar_base_datos
+from BK_CPSGR.compresion import comprimir_y_guardar_datos, cargar_y_descomprimir_datos
+from BK_CPSGR.servidor   import app, socketio
 
 def main():
     # Inicializar recursos
