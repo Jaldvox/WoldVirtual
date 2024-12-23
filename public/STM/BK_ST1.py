c59e4cf9e198cff8b4309d21bb7c805158f87bd2
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-23 14:54:15 +0000

    add

--- public/STM/BK_ST1.py
+++ public/STM/BK_ST1.py
@@ -1,9 +1,10 @@
 # principal.py
 
-from BK_ST2 import registrar_usuario
-from BK_ST3 import verificar_credenciales
-from BK_ST4 import manejar_accion
-from BK_Usuarios import inicializar_usuarios
+from STMDL.BK_ST2 import registrar_usuario
+from STMDL.BK_ST3 import verificar_credenciales
+from STMDL.BK_ST4 import manejar_accion
+from STMDL.BK_Usuarios import inicializar_usuarios
+from STMDL.__init__ import *
 
 if __name__ == '__main__':
     # Inicializar sistema de usuarios
