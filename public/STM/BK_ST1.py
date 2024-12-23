659b28dfbeda8d1ad0af4d9a70ae2ab77ee3f163
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-23 13:40:14 +0100

    Update BK_ST1.py

--- public/STM/BK_ST1.py
+++ public/STM/BK_ST1.py
@@ -3,3 +3,11 @@
 from BK_ST2 import registrar_usuario
 from BK_ST3 import verificar_credenciales
 from BK_ST3 import manejar_accion
+
+# Ejemplo de uso de registrar_usuario
+if __name__ == '__main__':
+    username = 'usuario1'
+    password = 'contraseña_segura'
+    registrar_usuario(username, password)
+    print(f'Usuario {username} registrado con éxito.')
+
