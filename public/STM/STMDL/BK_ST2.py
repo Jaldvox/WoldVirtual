07b6bd58ec1514fd64d7809729e46ae0b5dea6bc
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-23 13:25:12 +0100

    Update BK_ST2.py

--- public/STM/STMDL/BK_ST2.py
+++ public/STM/STMDL/BK_ST2.py
@@ -1,1 +1,5 @@
-# funciones ##
+usuarios = {}
+
+def registrar_usuario(username, password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    usuarios[username] = hashed_password
