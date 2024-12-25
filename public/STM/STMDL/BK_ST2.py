3a36d599e57797ba9acea1888118ce9a5f72ca1a
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-25 22:02:52 +0100

    Update BK_ST2.py

--- public/STM/STMDL/BK_ST2.py
+++ public/STM/STMDL/BK_ST2.py
@@ -1,16 +1,29 @@
-import hashlib
-from STMDL.BK_Usuarios import usuarios
-# from ST1MDL.BK_ST2_2
-# from ST1MDL.BK_ST2_3
-# from ST1MDL.BK_ST2_4
+from flask import Flask, jsonify, request
+import json
+import requests
+from BK_ST2_1 import cargar_blockchain, agregar_bloque, guardar_blockchain
+from BK_ST2_1_sincronizacion import sincronizar_blockchain
 
-def registrar_usuario(username, password):
-    """
-    Registra un nuevo usuario con su contraseña.
-    Devuelve True si el usuario fue registrado, False si ya existe.
-    """
-    if username in usuarios:
-        return False
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
-    usuarios[username] = hashed_password
-    return True
+app = Flask(__name__)
+
+# Cargar la blockchain localmente
+blockchain = cargar_blockchain()
+
+@app.route('/blockchain', methods=['GET'])
+def obtener_blockchain():
+    return jsonify(blockchain), 200
+
+@app.route('/nuevo_bloque', methods=['POST'])
+def agregar_bloque_r():
+    datos = request.get_json()
+    nuevo_bloque = datos['bloque']
+    blockchain.append(nuevo_bloque)  # Valida el bloque antes de agregarlo
+    guardar_blockchain(blockchain)
+    return "Bloque agregado", 200
+
+# Función para iniciar el servidor del nodo
+def iniciar_servidor():
+    app.run(port=5000)
+
+if __name__ == "__main__":
+    iniciar_servidor()
