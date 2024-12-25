9030e7d62c7735157da0048ee245f8980f793b53
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2024-12-25 22:33:51 +0100

    Update Bk_ST2_4.py

--- public/STM/STMDL/ST1MDL/Bk_ST2_4.py
+++ public/STM/STMDL/ST1MDL/Bk_ST2_4.py
@@ -1,13 +1,26 @@
-from BK_ST2_1_nodo import iniciar_servidor
-from BK_ST2_1_sincronizacion import sincronizar_blockchain
-from BK_ST2_1 import cargar_blockchain
+# BK_ST2_4.py - Funciones para la configuración y arranque del servidor del nodo
 
-def main():
-    # Sincronizar la blockchain con otros nodos
-    blockchain = sincronizar_blockchain()
-    
-    # Iniciar el servidor del nodo
-    iniciar_servidor()
+from flask import Flask, jsonify, request
+from BK_ST2_1 import cargar_blockchain, agregar_bloque
+from BK_ST2_3 import sincronizar_blockchain
 
-if __name__ == "__main__":
-    main()
+app = Flask(__name__)
+
+# Cargar la blockchain localmente
+blockchain = cargar_blockchain()
+
+@app.route('/blockchain', methods=['GET'])
+def obtener_blockchain():
+    return jsonify(blockchain), 200
+
+@app.route('/nuevo_bloque', methods=['POST'])
+def agregar_bloque_r():
+    datos = request.get_json()
+    nuevo_bloque = datos['bloque']
+    blockchain.append(nuevo_bloque)  # Valida el bloque antes de agregarlo
+    agregar_bloque(blockchain, nuevo_bloque['data'])
+    return "Bloque agregado", 200
+
+# Función para iniciar el servidor del nodo
+def iniciar_servidor():
+    app.run(port=5000)
