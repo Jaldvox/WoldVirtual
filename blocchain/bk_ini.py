cabcf486a885980896b2b58bf18306756788097c
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2025-05-29 19:39:10 +0000

    addbk

--- blocchain/bk_ini.py
+++ blocchain/bk_ini.py
@@ -17,23 +17,15 @@ class Blockchain:
         self.chain.append(block)
         return block
 
-    def mine_block(self, previous_proof):
+    def mine_new_block(self, previous_proof):
+        # Combines mine_block, proof_of_work and hash functions into one
         new_proof = 1
-        while hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()[:4] != '0000':
-            new_proof += 1
+        check_proof = lambda p: hashlib.sha256(str(p**2 - previous_proof**2).encode()).hexdigest()[:4] == '0000'
         
-        previous_block = self.chain[-1]
-        block = self.create_block(
-            proof=new_proof,
-            previous_hash=hashlib.sha256(str(previous_block).encode()).hexdigest()
-        )
-        return block
-    
-    def proof_of_work(self, previous_proof):
-        new_proof = 1
-        while hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()[:4] != '0000':
+        while not check_proof(new_proof):
             new_proof += 1
-        return new_proof
-    def hash(self, block):
-        encoded_block = str(block).encode()
-        return hashlib.sha256(encoded_block).hexdigest()
+            
+        previous_block = self.chain[-1]
+        new_hash = hashlib.sha256(str(previous_block).encode()).hexdigest()
+        
+        return self.create_block(proof=new_proof, previous_hash=new_hash)
